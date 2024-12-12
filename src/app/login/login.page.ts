f78e00351faaa3e4c937b07c4984b13f741f9f70
criticalRobin <matiastite1@gmail.com>
2024-12-12 14:58:06 -0500

    Redireccion a pagina home

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,21 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { User } from '../models/IUser';
 import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage  {
-user: User = { email: '', password: '' };
+export class LoginPage {
+  user: User = { email: '', password: '' };
 
   firebaseService = inject(FirebaseService);
+  router = inject(Router);
 
   async login() {
     try {
       const userCredential = await this.firebaseService.signIn(this.user);
-      console.log('Usuario logeado:', userCredential.user);
+
+      if (userCredential) {
+        this.router.navigate(['/home']);
+      }
+
       await this.firebaseService.saveUserEmail(this.user.email);
     } catch (error) {
       console.error('Error al iniciar sesión:', error);
